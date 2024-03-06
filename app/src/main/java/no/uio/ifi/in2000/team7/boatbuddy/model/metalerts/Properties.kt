e08a78e73fa36743165fef9ea0c9cec8ac127dff
ssensed <shannenf@uio.no>
2024-03-06 13:17:51 +0100

    add metalerts API

---
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/model/metalerts/Properties.kt
@@ -0,0 +1,24 @@
+package no.uio.ifi.in2000.team7.boatbuddy.model.metalerts
+
+data class Properties(
+    val area: String,
+    val awarenessResponse: String,
+    val awarenessSeriousness: String,
+    val awarenessLevel: String,
+    val awarenessType: String,
+    val certainty: String,
+    val consequences: String,
+    val county: List<Any>,
+    val description: String,
+    val event: String,
+    val eventAwarenessName: String,
+    val geographicDomain: String,
+    val id: String,
+    val instruction: String,
+    val resources: List<Resource>,
+    val riskMatrixColor: String,
+    val severity: String,
+    val title: String,
+    val triggerLevel: String,
+    val type: String
+)
