27dbedc189f12bc6a04926d92f07882cb8938912
aanundj@ulrik.uio.no <aanundj@ulrik.uio.no>
2024-03-07 19:04:09 +0100

    "Add LocationForecastScreen"

--- app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import no.uio.ifi.in2000.team7.boatbuddy.ui.metalerts.Screen
+import no.uio.ifi.in2000.team7.boatbuddy.ui.locationforecast.LocationForecastScreen
 import no.uio.ifi.in2000.team7.boatbuddy.ui.theme.BoatbuddyTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +21,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
 
                 ) {
-                    Screen()
+                    //Screen()
+                    LocationForecastScreen()
                 }
             }
         }
