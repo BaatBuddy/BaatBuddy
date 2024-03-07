c341134c613a3555a632a60410cefad755ee8808
aanundj@ulrik.uio.no <aanundj@ulrik.uio.no>
2024-03-07 18:43:57 +0100

    "Add LocationForecastViewModel"

--- app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/location_forecast/LocationForecastDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/location_forecast/LocationForecastDataSource.kt
@@ -1,4 +1,29 @@
 package no.uio.ifi.in2000.team7.boatbuddy.data.location_forecast
 
-class LocationForecastDataSource {
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.gson.gson
+import no.uio.ifi.in2000.team7.boatbuddy.data.location_forecast.dto.LocationForcastCompactDTO
+import java.net.UnknownHostException
+
+class LocationForecastDataSource(private val path: String = "url") {
+
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            gson()
+        }
+    }
+
+
+    suspend fun getLocationForecastData(): LocationForcastCompactDTO? {
+
+        return try {
+            val result: LocationForcastCompactDTO = client.get(path).body()
+            result
+        } catch (e: UnknownHostException) {
+            null
+        }
+    }
 }
--- app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/location_forecast/LocationForecastRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/location_forecast/LocationForecastRepository.kt
@@ -1,6 +1,16 @@
 package no.uio.ifi.in2000.team7.boatbuddy.data.location_forecast
 
-class LocationForecastRepository {
+import no.uio.ifi.in2000.team7.boatbuddy.data.location_forecast.dto.LocationForcastCompactDTO
+
+interface LocationForecastRepo {
+    suspend fun getLocationForecastData(): LocationForcastCompactDTO?
+}
+
+class LocationForecastRepository(private val dataSource: LocationForecastDataSource = LocationForecastDataSource()) :
+    LocationForecastRepo {
+    override suspend fun getLocationForecastData(): LocationForcastCompactDTO? {
+        return dataSource.getLocationForecastData()
+    }
 
 
 }
