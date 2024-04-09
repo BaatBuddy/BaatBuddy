e328da8e7e5919136ba9e1613d39531a1f4ce01a
Bsian02 <mariusfredrichsen@gmail.com>
2024-04-09 23:17:53 +0200

    data model modification and create object file - modify data model to be more alike - add all factors to the calulator

--- app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/WeatherScore.kt
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/WeatherScore.kt
@@ -1,23 +1,24 @@
 package no.uio.ifi.in2000.team7.boatbuddy.data
 
+import no.uio.ifi.in2000.team7.boatbuddy.model.locationforecast.TimeLocationData
+import no.uio.ifi.in2000.team7.boatbuddy.model.oceanforecast.TimeOceanData
+
 object WeatherScore {
 
 
     /*
     Things to include in the algorithm:
 
-    - oceanforecast
-        - wave_height
-        - sea_water_temperature
+        - wave_height                   - negativ
+        - sea_water_temperature         - positiv
 
-    - locationforecast
-        - wind_speed
-        - wind_speed_of_gust
-        - temperature
-        - cloud_area_fraction
-        - fog_area_fraction
-        - ultraviolet_index_clear_sky
-        - relative_humidity
+        - wind_speed                    - negativ
+        - wind_speed_of_gust            - negativ
+        - temperature                   - positiv
+        - cloud_area_fraction           - negativ
+        - fog_area_fraction             - negativ
+        - ultraviolet_index_clear_sky   - negativ
+        - relative_humidity             - negativ
 
     - sunrise
         - sunrise
@@ -25,6 +26,33 @@ object WeatherScore {
 
 
      */
+
+
+    // discuss if the end point should weigh the most
+
+    // creates a score from 0 - 100
+    fun calculatePoint(
+        oceanData: TimeOceanData,
+        forecastData: TimeLocationData
+    ): Double {
+        val waveHeight: Double = oceanData.sea_surface_wave_height
+        val waterTemperature: Double = oceanData.sea_water_temperature
+        val windSpeed: Double = forecastData.wind_speed
+        val windSpeedOfGust: Double = forecastData.wind_speed_of_gust
+        val airTemperature: Double = forecastData.air_temperature
+        val cloudAreaFraction: Double = forecastData.cloud_area_fraction
+        val fogAreaFraction: Double? = forecastData.fog_area_fraction
+        val ultravioletIndexClearSky: Double? = forecastData.ultraviolet_index_clear_sky
+        val relativeHumidity: Double = forecastData.relative_humidity
+
+        return 0.0
+    }
+
+    fun calculatePath(): Double {
+
+
+        return 0.0
+    }
 
 
 }
