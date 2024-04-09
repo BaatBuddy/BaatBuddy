b34d005305dfd3a9dfea2f6c2b8e69d120839ec7
Bsian02 <mariusfredrichsen@gmail.com>
2024-04-09 20:32:15 +0200

    - create list of potential factors in the algorithm

--- app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/WeatherScore.kt
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/WeatherScore.kt
@@ -1,4 +1,30 @@
 package no.uio.ifi.in2000.team7.boatbuddy.data
 
 object WeatherScore {
+
+
+    /*
+    Things to include in the algorithm:
+
+    - oceanforecast
+        - wave_height
+        - sea_water_temperature
+
+    - locationforecast
+        - wind_speed
+        - wind_speed_of_gust
+        - temperature
+        - cloud_area_fraction
+        - fog_area_fraction
+        - ultraviolet_index_clear_sky
+        - relative_humidity
+
+    - sunrise
+        - sunrise
+        - sunset
+
+
+     */
+
+
 }
