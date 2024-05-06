08b744bd0424bff52060d25b00726bedb990837a
Bsian02 <mariusfredrichsen@gmail.com>
2024-05-06 14:29:53 +0200

    still still dont know

--- app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/location/userlocation/UserLocationRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/location/userlocation/UserLocationRepository.kt
@@ -4,8 +4,16 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsResponse
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.Task
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -13,6 +21,7 @@ class UserLocationRepository @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
     private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+    private lateinit var locationRequest: LocationRequest
 
     fun getFusedLocationClient() = fusedLocationClient
     fun getContext() = context
@@ -30,5 +39,49 @@ class UserLocationRepository @Inject constructor(
 
     fun showLocationRequest(): Boolean {
         return false
+    }
+
+    fun buildThingy() {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationRequest = LocationRequest.Builder(5000)
+                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                .setMinUpdateIntervalMillis(1000)
+                .build()
+        }
+        // If coarse location is granted
+        else if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationRequest = LocationRequest.Builder(5000)
+                .setPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY)
+                .setMinUpdateIntervalMillis(1000)
+                .build()
+        }
+        // No permission granted
+        else {
+
+        }
+
+        val locationSettingsRequest = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+            .build()
+
+        val settingsClient = LocationServices.getSettingsClient(context)
+        val task: Task<LocationSettingsResponse> =
+            settingsClient.checkLocationSettings(locationSettingsRequest)
+        task.addOnCompleteListener {
+            if (task.isSuccessful) {
+                
+            } else {
+                // Handle exception?
+                val e = task.exception
+            }
+        }
     }
 }
--- app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/ui/home/UserLocationViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/ui/home/UserLocationViewModel.kt
@@ -1,9 +1,18 @@
 package no.uio.ifi.in2000.team7.boatbuddy.ui.home
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsResponse
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.Task
 import com.mapbox.geojson.Point
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
