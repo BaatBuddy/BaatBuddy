5df866855dd105323b288ac3d98bb12e731da582
shannenf <shannenf@uio.no>
2024-05-10 14:02:09 +0200

    change color, theme, logo, mapbox style

--- app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/mapbox/MapboxConstants.kt
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/data/mapbox/MapboxConstants.kt
@@ -3,5 +3,5 @@ package no.uio.ifi.in2000.team7.boatbuddy.data.mapbox
 object MapboxConstants {
     val token: String =
         "pk.eyJ1IjoibWFmcmVkcmkiLCJhIjoiY2x1MWIxZ3Q2MGtlZDJrbnhmdTZ0NHZtaSJ9.B6Iawg2wbjSnGqMEOEtxvQ"
-    val defaultStyle: String = "mapbox://styles/mafredri/clu8bbhvh019501p71sewd7eg"
+    val defaultStyle: String = "mapbox://styles/suio/clvgn1b0y018901qv7ht41s71"
 }
