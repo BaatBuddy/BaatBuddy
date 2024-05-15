8f10ca716b85144c339c5bdd5434c612fae0581d
Bsian02 <mariusfredrichsen@gmail.com>
2024-05-15 16:24:51 +0200

    homescreen bottomsheet explaination - add image to explain bottomsheet screen

--- app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/ui/onboarding/ExplainGenerateRouteScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team7/boatbuddy/ui/onboarding/ExplainGenerateRouteScreen.kt
@@ -1,19 +1,31 @@
 package no.uio.ifi.in2000.team7.boatbuddy.ui.onboarding
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -24,7 +36,12 @@ import androidx.compose.ui.unit.dp
 import no.uio.ifi.in2000.team7.boatbuddy.R
 
 @Composable
-fun ExplainGenerateRouteScreen() {
+fun ExplainGenerateRouteScreen(
+    modifier: Modifier = Modifier
+) {
+
+    val sliderPosition = remember { mutableStateOf(0.5f) } // range 0f to 1f
+
     Scaffold { paddingValue ->
         Box(
             modifier = Modifier
@@ -79,6 +96,13 @@ fun ExplainGenerateRouteScreen() {
                 )
 
                 // TODO picture of finished bottomsheet with correct colors
+                Image(
+                    painter = painterResource(id = R.drawable.homescreen_bottomsheet),
+                    contentDescription = "Hjemmeskjerm",
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                )
             }
         }
     }
